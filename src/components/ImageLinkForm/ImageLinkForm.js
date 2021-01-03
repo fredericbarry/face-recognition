89134805a2c51ef3d93bb314ac5a0671f953f58a
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2021-01-02 20:04:31 -0500

    Initial commit

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./ImageLinkForm.css";
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div>
+      <p className="f3 center">{"This app detects faces in pictures"}</p>
+      <div className="form center pa4 br3 shadow-5">
+        <input
+          className="f4 pa2 w-70 center"
+          type="text"
+          onChange={onInputChange}
+        />
+        <button
+          className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+          onClick={onButtonSubmit}
+        >
+          Detect
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ImageLinkForm;
